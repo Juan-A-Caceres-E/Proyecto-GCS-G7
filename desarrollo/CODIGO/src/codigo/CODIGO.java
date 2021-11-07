0b85ff2b801473005701f8ec1e49a74fb4d15b43
José Cuentas <72282358+restjcr@users.noreply.github.com>
2021-11-07 18:33:16 -0500

    a-commit

--- desarrollo/CODIGO/src/codigo/CODIGO.java
+++ desarrollo/CODIGO/src/codigo/CODIGO.java
@@ -15,7 +15,7 @@ public class CODIGO {
  * @param args the command line arguments
  */
 public static void main(String[] args) {
-    System.out.println("Esta es la rama de Jose Cuentas");
+    System.out.println("Codigo pertenciente a la rama jose_cuentas");
 }
 
 }
